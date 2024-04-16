b7404e52a3e502d387a299c9b57b8a6c3fe2c5ed
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-04-17 01:21:08 +0300

    add-js

--- src/js/lesson-22.js
+++ src/js/lesson-22.js
@@ -1,1 +1,15 @@
-console.log(test);
+/**
+Оголоси функцію calculateTotalPrice, яка рахує загальну суму покупки. 
+Вона приймає два параметри, значення яких будуть задаватися під час її виклику.
+
+orderedQuantity - перший параметр, кількість одиниць товару в замовленні;
+pricePerItem - другий параметр, ціна одиниці товару.
+Функція повинна повертати загальну суму покупки - результат множення кількості товарів 
+на ціну одного.
+ */
+
+function calculateTotalPrice(orderedQuantity, pricePerItem) {
+    return orderedQuantity * pricePerItem;
+}
+
+console.log(calculateTotalPrice(5, 2));
