f1857bd93a1ef15eac78944ccbe04833b00ed461
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-10-06 02:27:14 +0300

    Update lesson-22.js

--- src/js/lesson-22.js
+++ src/js/lesson-22.js
@@ -10,25 +10,50 @@
 
 // Приклад роботи https://ibb.co/6g3YYs8
 
-const day = 6;
+// const day = 6;
 
-switch (day) {
-  case 1:
-  case 2:
-  case 3:
-  case 4:
-  case 5:
-    console.log('This is a working day');
-    break;
-  case 6:
-  case 7:
-    console.log('It is a day off');
-    break;
-  default:
-    console.log('Invalid');
-}
+// switch (day) {
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log('This is a working day');
+//     break;
+//   case 6:
+//   case 7:
+//     console.log('It is a day off');
+//     break;
+//   default:
+//     console.log('Invalid');
+// }
 
 //задача
 
+// function isNumberInRange(start, end, number) {
+//   if(number >= start && number <= end) {return  true;
+// }else{return false;}
+// }
+
+// console.log(isNumberInRange(10, 30, 17));
+// console.log(isNumberInRange(10, 30, 5));
+// console.log(isNumberInRange(20, 50, 24));
+// console.log(isNumberInRange(20, 50, 76));
 
 
+function calculateEvenTotal(number) {
+  let total = 0;
+ for (let i = 0; i <= number; i += 1) {
+     if (i % 2 === 0) {
+         total += i;
+     }
+ }
+ return total;
+}
+
+
+console.log(calculateEvenTotal(1));
+console.log(calculateEvenTotal(3));
+console.log(calculateEvenTotal(7));
+console.log(calculateEvenTotal(18));
+console.log(calculateEvenTotal(27));
