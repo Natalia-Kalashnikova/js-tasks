677932672cdb93b792f793eaae5f41a5dc2a07ec
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-10-17 18:05:37 +0000

    Deploying to gh-pages from @ Natalia-Kalashnikova/js-tasks@7cfb889f9487e96bdc92875e39ebdc7deff6c134 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";function o(l,n){const r=[];for(let e=l;e<=n;e+=1)r.push(e);return r}console.log(o(1,3));console.log(o(14,17));console.log(o(29,34));console.log(o());
+import"./assets/modulepreload-polyfill-3cfb730f.js";function o(t,p,n){const e=parseFloat(t),l=parseFloat(p),s=parseFloat(n);return e+l*2+s*2}console.log(o("50px","8px","4px"));console.log(o("60px","12px","8.5px"));console.log(o("200px","0px","0px"));
 //# sourceMappingURL=commonHelpers2.js.map
