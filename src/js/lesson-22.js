7db1f252cad16c51a8fb04e89584758f72354c0f
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-10-23 01:16:29 +0300

    task 2.4

--- src/js/lesson-22.js
+++ src/js/lesson-22.js
@@ -53,13 +53,84 @@
 
 //задача 1.3
 
-function getElementWidth(content, padding, border ) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
-  return contentWidth + paddingWidth * 2 + borderWidth * 2;
+// function getElementWidth(content, padding, border ) {
+//   const contentWidth = parseFloat(content);
+//   const paddingWidth = parseFloat(padding);
+//   const borderWidth = parseFloat(border);
+//   return contentWidth + paddingWidth * 2 + borderWidth * 2;
+// }
+
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+//задача 2.1
+
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
+//   const totalPrice = quantity * pricePerDroid;
+//   if (totalPrice < customerCredits) {
+//     return `Insufficient funds!`;
+//   } else {
+//     return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+//   }
+// };
+
+
+// console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+// console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+// console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+//задача 2.2
+// function formatMessage(message, maxLength) {
+//   if (message.length <= maxLength) {
+//     return message;
+//   } else {
+//     return `${message.slice(0, maxLength)}...`;
+//   }
+// }
+
+// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+//задача 2.3
+// function checkForSpam(message) {
+//   const normalizeMessage = message.toLowerCase()
+//   if (normalizeMessage.includes("spam") || normalizeMessage.includes("sale")) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+// console.log(checkForSpam("Trust me, this is not a spam message")); // true
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+//задача 2.4
+function getShippingCost(country) {
+  switch (country) {
+    case "China": return `Shipping to China will cost 100 credits`;
+    case "Chile": return `Shipping to Chile will cost 250 credits`;
+    case "Australia": return `Shipping to Australia  will cost 170 credits`;
+    case "Jamaica": return `Shipping to Jamaica will cost 120 credits`;
+    default: return `Sorry, there is no delivery to your country`
+
+  }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
