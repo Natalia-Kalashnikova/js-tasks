91007bc8fbbca87246549d18f317983a53f11243
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-10-26 22:31:15 +0000

    Deploying to gh-pages from @ Natalia-Kalashnikova/js-tasks@8ae41ebff2fa5df29ada23c37c7e0442f684bffe 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";function o(e,r){let n=[];for(let l=0;l<e.length;l+=1)e[l]>r&&n.push(e[l]);return n}console.log(o([1,2,3,4,5],3));console.log(o([1,2,3,4,5],4));console.log(o([1,2,3,4,5],5));console.log(o([12,24,8,41,76],38));console.log(o([12,24,8,41,76],20));
+import"./assets/modulepreload-polyfill-3cfb730f.js";const e={descr:"Spacious apartment in the city center",rating:4,price:2153},t=Object.keys(e),c=Object.values(e);console.log(t);console.log(c);
 //# sourceMappingURL=commonHelpers2.js.map
