42de8c6031252dc47ec09e237db1f8899d599045
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-11-01 20:45:11 +0000

    Deploying to gh-pages from @ Natalia-Kalashnikova/js-tasks@be300f7774d7a77b05f0ea97bf585601d0dd418b 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";const t={potions:[{name:"Speed potion",price:460},{name:"Stone skin",price:520}],getPotions(){return this.potions},updatePotionName(n,i){for(const o of this.potions)o.name===n&&(o.name=i)}};t.updatePotionName("Stone skin","Invisibility");console.log(t.getPotions());
+import"./assets/modulepreload-polyfill-3cfb730f.js";function t(o){const n=Math.max(...o),e=Math.min(...o);return{best:n,worst:e}}console.log(t([89,64,42,17,93,51,26]));console.log(t([19,7,4,17,81,24]));
 //# sourceMappingURL=commonHelpers2.js.map
